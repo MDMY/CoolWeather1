93ad96aa53746598b38852ad58701b5e5c29ecd1
Ding <13160676651@163.com>
2016-06-13 18:09:16 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/ding/coolweather2/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/ding/coolweather2/activity/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.ding.coolweather2.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -57,6 +60,13 @@ public class ChooseAreaActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean("city_selected",false)){
+            Intent intent=new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
        // dbHelper=new CoolWeatherOpenHelper(this,"cool_weather.db",null,2);
@@ -74,6 +84,12 @@ public class ChooseAreaActivity extends Activity{
                 }else if (currentLevel==LEVEL_CITY){
                     selectedCity=cityList.get(index);
                     queryCounties();
+                }else if (currentLevel==LEVEL_COUNTY){
+                    String countyCode=countyList.get(index).getCountyCode();
+                    Intent intent=new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+                    intent.putExtra("county_code",countyCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
